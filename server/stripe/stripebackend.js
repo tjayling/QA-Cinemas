7d3028d2d9308e7e0b9168a4b9524a4e72d3e516
Jasmin <jwillis2@qa.com>
2022-07-04 15:05:46 +0100

    Fulfilled user story QAC-106 and task QAC-107. Added stripe functionality to payments

---
+++ server/stripe/stripebackend.js
@@ -0,0 +1,54 @@
+const cors = require("cors");
+const router = require("express").Router();
+const stripe = require("stripe")("sk_test_26PHem9AhJZvU623DfE1x4sd");
+//const uuid = require("uuid").v4;
+ 
+
+ 
+router.post("/checkout", async (req, res) => {
+  console.log("Request:", req.body);
+ 
+  let error;
+  let status;
+  try {
+    const { product, token } = req.body;
+ 
+    const customer = await stripe.customers.create({
+      email: token.email,
+      source: token.id,
+    });
+ 
+    const idempotency_key = uuid();
+    const charge = await stripe.charges.create(
+      {
+        amount: product.price * 100,
+        currency: "GBP",
+        customer: customer.id,
+        receipt_email: token.email,
+        description: `Purchased ${product.amount} cinema tickets`,
+        shipping: {
+          name: token.card.name,
+          address: {
+            line1: token.card.address_line1,
+            line2: token.card.address_line2,
+            city: token.card.address_city,
+            country: token.card.address_country,
+            postal_code: token.card.address_zip,
+          },
+        },
+      },
+      {
+        idempotency_key,
+      }
+    );
+    console.log("Charge:", { charge });
+    status = "success";
+  } catch (error) {
+    console.error("Error:", error);
+    status = "failure";
+  }
+ 
+  res.json({ error, status });
+});
+ 
+module.exports = router;
