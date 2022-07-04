dc1c4f76839684506c8ebd191e1580fe1fcccb90
tjayling <tjayling@yahoo.co.uk>
2022-07-04 15:11:17 +0100

    Fixed the comments page

--- client/src/pages/Forum.jsx
+++ client/src/pages/Forum.jsx
@@ -1,27 +1,12 @@
 import React from "react";
-import { useState } from "react";
 import Form from "../components/DiscussionsForm.jsx";
-import createElements from "../components/Format.jsx";
-const axios = require("axios");
+import CommentsFormat from "../components/CommentsFormat.jsx";
 
 const Forum = () => {
-  const [movieElements, setMovieElements] = useState([]);
-
-  axios
-    .get("http://localhost:3000/discussion")
-    .then((response) => setMovieElements(createElements((response.data), "comments")))
-    .catch((error) => console.error(error));
-
-  // let movieElements = [];
-  // function getViewings(data) {
-  //   // movieElements = createElements(data, "comments");
-  //   setMovieElements(createElements(data, "comment"));
-  // }
-
   return (
     <div style={{ display: "flex", justifyContent: "center" }}>
       <Form />
-      <div>{movieElements}</div>
+      <div>{CommentsFormat()}</div>
     </div>
   );
 };
