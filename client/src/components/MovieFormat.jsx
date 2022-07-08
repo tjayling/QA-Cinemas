a173adabf092a09ab5827ad04b43f0aa6f42a67d
guvmehat <gurveerm@hotmail.com>
2022-07-08 09:28:39 +0100

    way styling

--- client/src/components/MovieFormat.jsx
+++ client/src/components/MovieFormat.jsx
@@ -16,19 +16,10 @@ const MovieFormat = () => {
       <div className="movie-container" key={movie._id}>
         <img src={movie.img_link} alt={`${movie.title} cover`} width="200px" />
 
-<<<<<<< HEAD
-        <img class="item1" src={movie.img_link} alt={`${movie.title} cover`} width="200px" />
-
-        <div class="movie-text">
-        <h5 class="item2">{movie.title}</h5>
-        <h6 class="item3">Featuring {movie.actors}</h6>
-        <h6 class="item4">{movie.description}</h6>
-=======
         <div className="movie-data-container">
           <h3>{movie.title}</h3>
           <h4>Featuring {movie.actors}</h4>
           <p>{movie.description}</p>
->>>>>>> 9b2dc9e99213e2bebd4cb0d5f894b94fd774c811
         </div>
       </div>
     );
--- client/src/pages/Way.jsx
+++ client/src/pages/Way.jsx
@@ -4,22 +4,24 @@ import React from 'react'
 function Way() {
   return (
     <div>
-        <div class="columna">
+        <h1>How to Get here</h1>
+        <p>Whether your means of traving lis, we accommodate all types of trasnport.  </p>
+        <div class="col-6">
             <h1> Car</h1>
             <p>We have fanstaci parking infront of our cinema. Able to hold upto 420 cars, with 40 easy access parking and an addtional 50 quick charge spots. Parking is free. </p>
             <br></br>
         </div>
-        <div b class="columna">
+        <div b class="col-6">
             <h1> Bus</h1>
             <p>The 51 connects the city centre to the cinema. Operating 24hours allowing you to watch your favorite movie at midnight luanches</p>
             <br></br>
         </div>
-        <div c class="columnb">
+        <div c class="col-6">
             <h1> Cycle</h1>
             <br></br>
             <p>The A1 has a dual cycle path keeping you safe and off the road, or travel the scince route on the canel, 16b north Indoor bike storage which is looked after by employees to keep your bike safe.</p>
         </div>
-        <div d class="columnb">
+        <div d class="col-6">
             <h1> Walking</h1>
             <br></br>
             <p> Situated 20 minuites from the city centre, 25 minuites from south city center </p>
