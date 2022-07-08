eead656a1ced19dfa76ad70ff468ee0c5b1b9277
tjayling <tjayling@yahoo.co.uk>
2022-07-08 10:08:43 +0100

    Added a page for individual movies and delete buttons to each comment in the discussion page

--- client/src/components/MovieFormat.jsx
+++ client/src/components/MovieFormat.jsx
@@ -1,34 +1,35 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import "../pages/Style/Movies.css";
 
 const axios = require("axios");
 
-const MovieFormat = () => {
+const MovieFormat = (id) => {
   const [movieData, setMovieData] = useState([]);
   useEffect(() => {
-    axios
-      .get("http://localhost:3000/movies")
-      .then((response) => setMovieData(response.data.data))
-      .catch((error) => console.error(error));
+    id
+      ? axios
+          .get(`http://localhost:3000/movies/${id}`)
+          .then((response) => setMovieData([...movieData, response.data.data]))
+          .catch((error) => console.error(error))
+      : axios
+          .get("http://localhost:3000/movies")
+          .then((response) => setMovieData(response.data.data))
+          .catch((error) => console.error(error));
   }, []);
   return movieData.map((movie) => {
     return (
       <div className="movie-container" key={movie._id}>
-        <img src={movie.img_link} alt={`${movie.title} cover`} width="200px" />
-
-<<<<<<< HEAD
-        <img class="item1" src={movie.img_link} alt={`${movie.title} cover`} width="200px" />
-
-        <div class="movie-text">
-        <h5 class="item2">{movie.title}</h5>
-        <h6 class="item3">Featuring {movie.actors}</h6>
-        <h6 class="item4">{movie.description}</h6>
-=======
+        <img
+          className="movie-cover-img"
+          src={movie.img_link}
+          alt={`${movie.title} cover`}
+          width="200px"
+        />
         <div className="movie-data-container">
-          <h3>{movie.title}</h3>
-          <h4>Featuring {movie.actors}</h4>
-          <p>{movie.description}</p>
->>>>>>> 9b2dc9e99213e2bebd4cb0d5f894b94fd774c811
+          <h2 className="movie-title">{movie.title}</h2>
+          <h4 className="movie-data">Featuring {movie.actors}</h4>
+          <p className="movie-data">{movie.description}</p>
         </div>
       </div>
     );
--- client/src/pages/Viewing.jsx
+++ client/src/pages/Viewing.jsx
@@ -125,7 +125,6 @@ const Viewing = () => {
             </select>
           </div>
         </div>
-        <Footer />
       </div>
     </div>
   );
